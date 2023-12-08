6a5ee8abaf4b7ceda31aeccf1a71404fd5209900
Роман Мифтахов <miftahov9548@gmail.com>
2023-12-08 13:25:57 +0300

    dsad

---
+++ Application_for_the_Client/ModelViews/Main_Catalog_Window_ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Application_for_the_Client.Models;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_for_the_Client.ModelViews
+{
+    internal class Main_Catalog_Window_ViewModel : ViewModelBase
+    { 
+        public ObservableCollection<ProductModel> Tovar { get; set; }
+        public ProductModel SelectedItem { get; set; }
+        public Main_Catalog_Window_ViewModel()
+        {
+            Tovar = new ObservableCollection<ProductModel>();
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
+            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+        }
+    }
+}
---
+++ Application_for_the_Client/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_for_the_Client.Models
+{
+    internal class ProductModel
+    {
+        public int Id { get; set; }
+        public string Productname { get; set; }
+        public int Productprice { get; set; }
+    }
+}
