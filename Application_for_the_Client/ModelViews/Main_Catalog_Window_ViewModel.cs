24728b353793f5025ab1c560d5485a8cb3e0fdec
Роман Мифтахов <miftahov9548@gmail.com>
2023-12-08 14:42:38 +0300

    GGG

--- Application_for_the_Client/ModelViews/Main_Catalog_Window_ViewModel.cs
+++ Application_for_the_Client/ModelViews/Main_Catalog_Window_ViewModel.cs
@@ -23,22 +23,17 @@ namespace Application_for_the_Client.ModelViews
             Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
             Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
             Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
-            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
-            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
-            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
-            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
-            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
-            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
-            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
-            Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });     Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
             Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
             Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
             Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
             Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
             Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
             Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
+            Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });Tovar.Add(new ProductModel { Id = 1, Productname = "Товар 1", Productprice = 100 });
             Tovar.Add(new ProductModel { Id = 2, Productname = "Товар 2", Productprice = 200 });
-            Tovar.Add(new ProductModel { Id = 3, Productname = "Товар 3", Productprice = 300 });
+          
         }
     }
 }
